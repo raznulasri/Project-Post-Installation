ffcd912b6c6d28b6b1a63f54fff782015c1ad7c2
raznulasri <raznulasri@gmail.com>
2026-09-09 12:39:26 +0800

    disable-selinux

--- pre-installation.sh
+++ pre-installation.sh
@@ -1,2 +1,5 @@
 echo install bash-completion
 yum install bash-completion -y
+
+echo disabled selinux. restart required
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config && sudo setenforce 0
