18f9e512096d75859fe62fdb50c483cbc62e2d90
raznulasri <raznulasri@gmail.com>
2026-09-10 17:10:53 +0800

    edit a9

--- a9-pre-installation.sh
+++ a9-pre-installation.sh
@@ -148,16 +148,15 @@ read -p "Do you want to proceed with cPanel installation? (yes/no): " confirm
 if [[ "$confirm" != "yes" ]]; then
   echo "[-] cPanel installation skipped."
 else
+  cd /home
+  curl -o latest -L https://securedownloads.cpanel.net/latest
+  sh latest
 
-cd /home
-curl -o latest -L https://securedownloads.cpanel.net/latest
-sh latest
-
-echo
-echo "=========================================="
-echo " Running Force Update on cPanel"
-echo "=========================================="
-/usr/local/cpanel/scripts/upcp --force
+  echo
+  echo "=========================================="
+  echo " Running Force Update on cPanel"
+  echo "=========================================="
+  /usr/local/cpanel/scripts/upcp --force
 fi
 
 echo
