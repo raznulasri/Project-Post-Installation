8f87e4eadaf08d3ff88c91726949bc2f8e4343c4
raznulasri <raznulasri@gmail.com>
2026-09-11 15:08:48 +0800

    multios edit

--- MultiLinuxOS.sh
+++ MultiLinuxOS.sh
@@ -193,3 +193,4 @@ hostnamectl set-hostname RaznulAsri
 echo
 echo "IMPORTANT: Please reboot your server to apply all kernel and SELinux changes."
 echo "Command: sudo reboot"
+rm -rf MultiLinuxOS.sh
